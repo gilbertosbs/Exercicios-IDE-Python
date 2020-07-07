4009571fc3b7b53a0047516a50a336f9d80b98c9
gilberto aparecido pereira <49577657+gilbertosbs@users.noreply.github.com>
2020-07-06 22:38:44 -0300

    Update salarioliquidoesalariobase.py

--- salarioliquidoesalariobase.py
+++ salarioliquidoesalariobase.py
@@ -5,6 +5,7 @@ pd=float(input("Digite o porcentual de descontos:"))
 sb=ht*vh
 td=(pd/100)*sb
 sl=sb-td
+#a cesta básica está demonstrada abaixo como 120
 print("O salario base + cesta básica é de %5.2f" % (sb+120))
 print("O salario liquido é de %3.2f" % sl)
 
